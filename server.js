585554eaac2019f022ba3a39d12e78f349504a93
fardos <khalailyt@gmail.com>
2021-10-18 11:19:11 +0300

    make it better

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/i3h'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/i3h/index.html'));});
 app.listen(process.env.PORT || 8080);
