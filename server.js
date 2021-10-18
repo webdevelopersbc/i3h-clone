047f5ab1fc253ef9f7221d3126fa7972a5f26235
Geekboots <info@geekboots.com>
2021-10-18 21:17:20 +0530

    Update server.js
    updated

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/nhlapi'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/i3h/index.html'));});
+'/dist/nhlapi/index.html'));});
 app.listen(process.env.PORT || 8080);
