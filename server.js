2de7bd7224e907251b254d17b47ca0278a55c9a3
fardos <khalailyt@gmail.com>
2021-10-18 11:12:26 +0300

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ןi3h'));
+app.use(express.static(__dirname + '/dist/i3h'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/myapp/index.html'));});
